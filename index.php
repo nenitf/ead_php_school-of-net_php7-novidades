9d26a36b700a228cccf2388e4deebdc6c8583fd0
Felipe Silva <felipe.dacs@gmail.com>
2019-10-10 20:59:01 -0300

    release: add aula 5

--- index.php
+++ index.php
@@ -1,30 +1,10 @@
 <?php
+// Null Coalescing Operator
 
-class Exemplo1
-{
+// http://localhost:8080/
+// http://localhost:8080/?nome=Felipe
+$nome = $_GET["nome"] ?? "Anônimo";
+// $nome = isset($_GET["nome"]) ? $_GET["nome"] : "Anônimo";
 
-}
+echo "Hello $nome";
 
-class Exemplo2
-{
-    /* Tipos:
-     * array
-     * callable (funções)
-     *
-     ***** PHP 7
-     * bool
-     * float
-     * int
-     * String
-     */
-    // function name(TipoParametro Var):TipoRetorno
-    function get(Exemplo1 $v):bool
-    {
-        // ...
-    }
-}
-
-/**
- * Caso não seja retornado o tipo correto, o php tenta converter
- * A menos que seja utilizado: declare(strict_types=1);
- */
