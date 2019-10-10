168a4f6e0bed81317fbd9dd0d38fdea069debc37
Felipe Silva <felipe.dacs@gmail.com>
2019-10-10 20:40:48 -0300

    release: add aula 2

--- index.php
+++ index.php
@@ -1,17 +1,24 @@
 <?php
 
-// força tipagem no php
-declare(strict_types=1); 
+class Exemplo1
+{
 
-$php7 = function(String $title):array{
-    return [
-        $title."\n",
-        "PHP on another level",
-        "More perforance",
-        "2x faster"
-    ];
-};
+}
 
-foreach($php7("News in PHP 7") as $p) {
-    echo "$p\n";
+class Exemplo2
+{
+    /* Tipos:
+     * array
+     * callable (funções)
+     *
+     ***** PHP 7
+     * bool
+     * float
+     * int
+     * String
+     */
+    function get(Exemplo1 $v)
+    {
+        // ...
+    }
 }
