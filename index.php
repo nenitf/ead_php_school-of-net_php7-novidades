1b6ab84010bdcc86f7f6fed05e9240964459be33
Felipe Silva <felipe.dacs@gmail.com>
2019-10-11 00:38:33 -0300

    release: add aula 11

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
 <?php
-// Filtered unserialize();
+/*
+ * Antes:
+ * use foo\bar\exemplo1;
+ * use foo\bar\exemplo2;
+ * use foo\bar\exemplo3 as E3;
+ *
+ * Depos:
+ * use foo\bar\{exemplo1, exemplo2 exemplo3 as E3};
+ * ou
+ * use foo\bar\{i
+ *      exemplo1, exemplo2 exemplo3 as E3
+ * };
+ */
