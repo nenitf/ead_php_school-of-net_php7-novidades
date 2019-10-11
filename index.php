e9ae06252c28ddffef374ec14bcfb9a0820ae71d
Felipe Silva <felipe.dacs@gmail.com>
2019-10-10 21:19:32 -0300

    release: add aula 7

--- index.php
+++ index.php
@@ -1,24 +1,10 @@
 <?php
-// Spaceship
+// Constantes com array
 
-/**
- * Iguais retorna 0
- * Esquerda maior retorna -1
- * Direita maior retorna 1
- */
+define('ANIMALS', [
+    "dog",
+    "cat",
+    "bird"
+]);
 
-echo 1<=>1; // 0
-echo 2<=>2; // 0
-echo 3<=>3; // 0
-echo 2.1<=>4.2/2; // 0
-
-echo 1<=>2; // -1
-echo 1<=>7; // -1
-echo 1<=>9; // -1
-echo "a"<=>"b"; // -1
-echo [1,2]<=>[1,2,3]; // -1
-
-echo 2<=>1; // 1
-echo 5<=>1; // 1
-echo 9<=>6; // 1
-echo [1,2,4]<=>[1,2,3]; // 1
+var_dump(ANIMALS);
