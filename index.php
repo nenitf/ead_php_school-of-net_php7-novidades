3f084ad856625f43af90e37936abbcb4a3eadb00
Felipe Silva <felipe.dacs@gmail.com>
2019-10-10 21:17:23 -0300

    release: add aula 6

--- index.php
+++ index.php
@@ -1,10 +1,24 @@
 <?php
-// Null Coalescing Operator
+// Spaceship
 
-// http://localhost:8080/
-// http://localhost:8080/?nome=Felipe
-$nome = $_GET["nome"] ?? "Anônimo";
-// $nome = isset($_GET["nome"]) ? $_GET["nome"] : "Anônimo";
+/**
+ * Iguais retorna 0
+ * Esquerda maior retorna -1
+ * Direita maior retorna 1
+ */
 
-echo "Hello $nome";
+echo 1<=>1; // 0
+echo 2<=>2; // 0
+echo 3<=>3; // 0
+echo 2.1<=>4.2/2; // 0
 
+echo 1<=>2; // -1
+echo 1<=>7; // -1
+echo 1<=>9; // -1
+echo "a"<=>"b"; // -1
+echo [1,2]<=>[1,2,3]; // -1
+
+echo 2<=>1; // 1
+echo 5<=>1; // 1
+echo 9<=>6; // 1
+echo [1,2,4]<=>[1,2,3]; // 1
