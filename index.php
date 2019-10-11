2968d88e6e2b8af68e42083ea2b63b4e78b046b4
Felipe Silva <felipe.dacs@gmail.com>
2019-10-11 00:07:13 -0300

    release: add aula 10

--- index.php
+++ index.php
@@ -1,24 +1,2 @@
 <?php
-// Closure::call();
-
-class Exemplo
-{
-    private $x = 1;
-}
-
-/////////////////////////////////////////// Antes
-$getABC = function()
-{
-    return $this->x;
-};
-
-$getX = $getABC->bindTo(new Exemplo, "Exemplo");
-echo $getX();
-
-/////////////////////////////////////////// Depois
-$getX2 = function()
-{
-    return $this->x;
-};
-echo $getX->call(new Exemplo);
-// echo $getABC->call(new Exemplo);
+// Filtered unserialize();
